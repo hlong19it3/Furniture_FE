21b04b305e9c2302ee6e0b7067260289fd8a5ded
hlong19it3 <hlong.19it3@vku.udn.vn>
2022-12-08 11:07:37 +0700

    Add search Order, Category

--- src/Pages/Admin/CategoriesPage/CategoryPage.js
+++ src/Pages/Admin/CategoriesPage/CategoryPage.js
@@ -1,15 +1,28 @@
 import { useEffect, useState } from 'react';
 import CustomAxios from '~/config/api';
+import useDebounce from '~/hooks/useDebounce';
 
 function CategoryPage() {
   // const accessToken = localStorage.getItem();
   // axios.interceptors.request.use()
 
   const [categories, setCategory] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const debounced = useDebounce(searchValue, 600);
+
   useEffect(() => {
     getCategories();
     // eslint-disable-next-line
   }, []);
+
+  useEffect(() => {
+    if (!debounced.trim()) {
+      getCategories();
+    } else {
+      searchCategory(debounced);
+    }
+    // eslint-disable-next-line
+  }, [debounced]);
 
   const tokens = JSON.parse(localStorage.getItem('userInfo'));
 
@@ -17,6 +30,7 @@ function CategoryPage() {
     const res = await CustomAxios.get('/api/v1/categories/', { headers: { 'x-accesstoken': tokens.accessToken } });
     setCategory(res.data);
   };
+
   const deleteCategory = async (id) => {
     try {
       await CustomAxios.delete(`/api/v1/categories/${id}`, { headers: { 'x-accesstoken': tokens.accessToken } });
@@ -26,6 +40,22 @@ function CategoryPage() {
     }
   };
 
+  const searchCategory = async (value) => {
+    try {
+      const res = await CustomAxios.get(`/api/v1/categories/search/${value}`, {
+        headers: { 'x-accesstoken': tokens.accessToken },
+      });
+
+      setCategory(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSearchCategory = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className=" flex  flex-1 justify-center items-center p-10">
       <div className=" w-full relative shadow-md sm:rounded-lg ">
@@ -33,32 +63,14 @@ function CategoryPage() {
           <div className="mb-3 xl:w-96">
             <div className="input-group relative flex flex-wrap items-stretch w-full mb-4 rounded">
               <input
+                value={searchValue}
+                onChange={handleSearchCategory}
                 type="search"
                 className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 placeholder="Search"
                 aria-label="Search"
                 aria-describedby="button-addon2"
               />
-              <span
-                className="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-gray-700 text-center whitespace-nowrap rounded"
-                id="basic-addon2"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="search"
-                  className="w-4"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-                  ></path>
-                </svg>
-              </span>
             </div>
           </div>
         </div>
